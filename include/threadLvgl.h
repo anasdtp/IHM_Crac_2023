cb5bdc76db24c41f33dcdc4374c15579132fe79d
Yves Guinand <yves.guinand@universite-paris-saclay.fr>
2023-03-23 22:53:56 +0100

    Réécriture dans ThreadLvgl

--- include/threadLvgl.h
+++ include/threadLvgl.h
@@ -2,6 +2,7 @@
 #define __THREADLVGL_H
 
 #include "mbed.h"
+#include "lvgl.h"
 
 class ThreadLvgl
 {
@@ -12,10 +13,19 @@ protected:
         p->runLvgl();
     }
     Thread m_mainThread;
-    void touchpadInit();  
+    void displayInit();
+    static void refreshDisplay(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p);
+    void touchpadInit();
+    static void touchpadRead(lv_indev_drv_t *indev_drv, lv_indev_data_t *data);
+    lv_indev_t *indevTouchpad;    
+    static bool refreshEnabled;
+    Ticker lvTicker;
+    static int refreshTime;
+    static void lvTimeCounter();
+    void setRefreshEnable(bool enabled = true) { refreshEnabled = enabled; }
 public:
     Mutex mutex;
-    ThreadLvgl();
+    ThreadLvgl(int refreshTimeInit = 5);
     ~ThreadLvgl();
 };
 
--- src/threadLvgl.cpp
+++ src/threadLvgl.cpp
@@ -1,16 +1,18 @@
 #include "threadLvgl.h"
 #include "../BSP_DISCO_F469NI/Drivers/STM32469I-Discovery/stm32469i_discovery_lcd.h"
 #include "../BSP_DISCO_F469NI/Drivers/STM32469I-Discovery/stm32469i_discovery_ts.h"
-#include "lvgl.h"
-#include "lv_port_disp.h"
+#include "../lvgl/src/draw/stm32_dma2d/lv_gpu_stm32_dma2d.h"
 
-lv_indev_t * indev_touchpad;
+bool ThreadLvgl::refreshEnabled = true;
+int ThreadLvgl::refreshTime = 5;
 
-ThreadLvgl::ThreadLvgl()
+ThreadLvgl::ThreadLvgl(int refreshTimeInit)
 {
     lv_init();
-    lv_port_disp_init();
-    touchpadInit();    
+    displayInit();
+    touchpadInit();
+    refreshTime = refreshTimeInit;
+    lvTicker.attach(lvTimeCounter, chrono::milliseconds(refreshTime));
     m_mainThread.start(callback(ThreadLvgl::run, this));
 }
 
@@ -19,10 +21,15 @@ ThreadLvgl::~ThreadLvgl()
     m_mainThread.join();
 }
 
+void ThreadLvgl::lvTimeCounter()
+{
+    lv_tick_inc(refreshTime);
+}
+
 void ThreadLvgl::runLvgl()
 {
     while (1) {
-        lv_tick_inc(5); 
+        //lv_tick_inc(5); 
         //Call lv_tick_inc(x) every x milliseconds in a Timer or Task (x should be between 1 and 10). 
         //It is required for the internal timing of LittlevGL.
         mutex.lock();
@@ -30,54 +37,89 @@ void ThreadLvgl::runLvgl()
         mutex.unlock(); 
         //Call lv_task_handler() periodically every few milliseconds. 
         //It will redraw the screen if required, handle input devices etc.
-        ThisThread::sleep_for(5ms);
+        ThisThread::sleep_for(chrono::milliseconds(refreshTime));
     }
 }
 
-// extern LTDC_HandleTypeDef  hltdc_eval;
+void ThreadLvgl::displayInit(void)
+{
+    /*-------------------------
+     * Initialize your display
+     * -----------------------*/
+    /*You code here*/
+    // Init the touch screen display via the BSP driver. Based on ST's example.
+    BSP_LCD_Init();
+    BSP_LCD_LayerDefaultInit(0, LCD_FB_START_ADDRESS);
+    BSP_LCD_DisplayOn();
+    BSP_LCD_SelectLayer(0);
+    
+    /*-----------------------------
+     * Create a buffer for drawing
+     *----------------------------*/
 
-// void my_disp_flush_cb(lv_disp_drv_t* disp_drv, const lv_area_t* area, lv_color_t* color_p)
-// {
-//     //The most simple case (but also the slowest) to put all pixels to the screen one-by-one
-//     uint16_t x, y;
-//  /*   for(y = area->y1; y <= area->y2; y++) {
-//         for(x = area->x1; x <= area->x2; x++) {
-//             //put_px(x, y, *color_p)
-//             BSP_LCD_DrawPixel( x, y, color_p->full);
-//             color_p++;
-//         }
-//     }
-// */
-// /*
-//     uint32_t *c = (uint32_t *)(color_p);
-//     for(y = area->y1; y <= area->y2; y++) {
-//         uint32_t *p = (uint32_t *)(hltdc_eval.LayerCfg[LTDC_ACTIVE_LAYER_BACKGROUND].FBStartAdress + (4*(y*BSP_LCD_GetXSize() + area->x1)));
-//         for(x = area->x1; x <= area->x2; x++) {
-//             *p = *c;
-//             p++;
-//             c++;
-//         }
-//     }
-// */
-//     uint8_t *c = (uint8_t *)(color_p);
-//     int d = (area->x2 - area->x1 + 1)*4;
-//     uint32_t *p = (uint32_t *)(hltdc_eval.LayerCfg[LTDC_ACTIVE_LAYER_BACKGROUND].FBStartAdress + (4*(area->y1*BSP_LCD_GetXSize() + area->x1)));
-//     for(y = area->y1; y <= area->y2; y++) {
-//         memcpy(p, c, d);
-//         p+=800;
-//         c+=d;
-//     }
+    /**
+     * LVGL requires a buffer where it internally draws the widgets.
+     * Later this buffer will passed to your display driver's `flush_cb` to copy its content to your display.
+     * The buffer has to be greater than 1 display row
+     * 
+     * 2. Create TWO buffer:
+     *      LVGL will draw the display's content to a buffer and writes it your display.
+     *      You should use DMA to write the buffer's content to the display.
+     *      It will enable LVGL to draw the next part of the screen to the other buffer while
+     *      the data is being sent form the first buffer. It makes rendering and flushing parallel.
+     */
+    lv_color_t *buf0 = (lv_color_t *)(LCD_FB_START_ADDRESS+0x00200000); /*A screen sized buffer*/
+    lv_color_t *buf1 = (lv_color_t *)(LCD_FB_START_ADDRESS+0x00400000); /*Another screen sized buffer*/
+    static lv_disp_draw_buf_t draw_buf_dsc;
+    lv_disp_draw_buf_init(&draw_buf_dsc, buf0, buf1,
+                          BSP_LCD_GetXSize() * BSP_LCD_GetYSize()); /*Initialize the display buffer*/
 
-//     //IMPORTANT!!!* Inform the graphics library that you are ready with the flushing
-//     lv_disp_flush_ready(disp_drv);
-// }
+    /*-----------------------------------
+     * Register the display in LVGL
+     *----------------------------------*/
+
+    static lv_disp_drv_t disp_drv; /*Descriptor of a display driver*/
+    lv_disp_drv_init(&disp_drv);   /*Basic initialization*/
+
+    /*Set up the functions to access to your display*/
+
+    /*Set the resolution of the display*/
+    disp_drv.hor_res = BSP_LCD_GetXSize();
+    disp_drv.ver_res = BSP_LCD_GetYSize();
+
+    /*Used to copy the buffer's content to the display*/
+    disp_drv.flush_cb = refreshDisplay;
+
+    /*Set a display buffer*/
+    disp_drv.draw_buf = &draw_buf_dsc;
+
+    /*Finally register the driver*/
+    lv_disp_drv_register(&disp_drv);
+}
+
+/*Flush the content of the internal buffer the specific area on the display
+ *You can use DMA or any hardware acceleration to do this operation in the background but
+ *'lv_disp_flush_ready()' has to be called when finished.*/
+void ThreadLvgl::refreshDisplay(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
+{
+    if (refreshEnabled)
+    {
+        lv_color_t *fbp32 = (lv_color_t *)LCD_FB_START_ADDRESS;
+        lv_draw_stm32_dma2d_buffer_copy(NULL, fbp32 + area->y1 * LV_HOR_RES + area->x1,
+        	(lv_coord_t) LV_HOR_RES, area, color_p, area->x2 - area->x1 + 1, area);
+    }
+
+    /*IMPORTANT!!!
+     *Inform the graphics library that you are ready with the flushing*/
+    lv_disp_flush_ready(disp_drv);
+}
 
 /*------------------
  * Touchpad
  * -----------------*/
 
 /*Will be called by the library to read the touchpad*/
-static void touchpad_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data)
+void ThreadLvgl::touchpadRead(lv_indev_drv_t * indev_drv, lv_indev_data_t * data)
 {
     static lv_coord_t last_x = 0;
     static lv_coord_t last_y = 0;
@@ -113,6 +155,6 @@ void ThreadLvgl::touchpadInit(void){
     /*Register a touchpad input device*/
     lv_indev_drv_init(&indev_drv);
     indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = touchpad_read;
-    indev_touchpad = lv_indev_drv_register(&indev_drv);
+    indev_drv.read_cb = touchpadRead;
+    indevTouchpad = lv_indev_drv_register(&indev_drv);
 }
